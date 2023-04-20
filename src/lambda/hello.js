7ff22fa4fb7ca443b5c4fa9f185cb17e9cb298aa
Filipe Gomes Silva <a2022113664@isec.pt>
2023-04-20 22:57:47 +0100

    call

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -3,6 +3,6 @@ export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters)
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, Guilherme!' }),
+    body: JSON.stringify({ msg: 'Vamos ao Cobra?' }),
   })
 }
